bf8e8108f6b7d28c3196a7aa698a7897143d5cab
TheBerberiCode <berberi.safet@gmail.com>
2016-02-21 00:19:30 -0500

    added user schema

---
+++ models/user.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+var bcrypt = require('brcypt-nodejs');
+var Schema = mongoose.Schema;
+
+//user schema 
+var UserSchema = new mongoose.Schema({
+	email: {type: String, unique: true, lowercase: true},
+	password: String,
+
+	profile: {
+		name: {type: String, default: ''},
+		picture: {type: String, default: ''}
+	},
+
+	address: String,
+	history: [{
+		date: Date,
+		paid: {type: Number, default: 0},
+		item: {type: Schema.Types.ObjectrId, ref: ''}
+	}]
+});
+
+//hash pass
+
+UserSchema.pre('save', function(next) {
+	var user = this;
+
+	if(!user.isModified('password')) return next();
+	bcrypt.genSalt(10, function(err,salt) {
+		if(err) {
+			return next(err);
+		}
+
+		brycpt.hash(user.password, salt, null, function (hash) {
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+//compare db pass with typed pass
+UserSchema.methods.comparePassword = function (password) {
+	return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 var express = require('express');
+var morgan = require('morgan');
+
 var app = express();
 
-
+// middleware
+app.use(morgan('dev'));
 
 
 app.get('/', function(req,res) {
 	var name =  "Safetyyy";
 	res.json("My name is " + name);
 });
-
-
-
-
 
 app.listen(3000, function(err) {
 	if(err) {
