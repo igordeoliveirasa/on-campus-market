f6b03be974ddc571263f2165d64dd8f21187648a
TheBerberiCode <berberi.safet@gmail.com>
2016-02-22 13:19:40 -0500

    added mongoose functionality

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect('mongodb://root:abc123@ds013908.mongolab.com:13908/oncampusmarket', function(err) {
+	if(err) {
+		console.log(err);
+	}else {
+		console.log("Coonnected to the database");
+	}
+});
 
 // middleware
 app.use(morgan('dev'));
