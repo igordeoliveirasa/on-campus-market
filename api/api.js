61c99685ba5cafda5743792a9c1faafdee4f4b54
TheBerberiCode <berberi.safet@gmail.com>
2016-03-03 00:18:59 -0500

    added procfile for production

--- api/api.js
+++ api/api.js
@@ -5,21 +5,23 @@ var Category = require('../models/category');
 var Product = require('../models/product');
 
 
-router.get('/:name', function (req,res,next) {
+router.get('/:name', function(req, res, next) {
 	async.waterfall([
 
 		function(callback) {
-			Category.findOne({name:req.params.name}, function(err, category) {
-				if(err) return next(err);
+			Category.findOne({
+				name: req.params.name
+			}, function(err, category) {
+				if (err) return next(err);
 				callback(null, category);
 			});
-		}, 
+		},
 
-		function (category, callback) {
-			for(var i=0; i<30; i++) {
+		function(category, callback) {
+			for (var i = 0; i < 30; i++) {
 				var product = new Product();
 				product.category = category._id;
-				product.name = faker.commerce.productName();//fake data for testing
+				product.name = faker.commerce.productName(); //fake data for testing
 				product.price = faker.commerce.price();
 
 				product.image = faker.image.image();
@@ -28,9 +30,11 @@ router.get('/:name', function (req,res,next) {
 			}
 		}
 
-		]);
- 
- res.json({message:'success'});
+	]);
+
+	res.json({
+		message: 'success'
+	});
 });
 
 
