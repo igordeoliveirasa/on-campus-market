80e4b6343adb5f4730746c1318e365d4099db4bb
TheBerberiCode <berberi.safet@gmail.com>
2016-03-04 14:40:54 -0500

    added ability to search using elasticsearch and navbar search input

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,60 @@ var router = require('express').Router();
 var User = ('../models/users');
 var Product = require('../models/product');
 
+
+
+//maps elastic search replica set with the product schema
+Product.createMapping(function(err, mapping) {
+	if (err) {
+		console.log("Error creating mapping");
+		console.log("err");
+	} else {
+		console.log("Mapping created");
+		console.log(mapping);
+	}
+});
+
+var stream = Product.synchronize();
+var count = 0;
+
+stream.on('data', function() {
+	count++;
+});
+
+//counts all the documents once the stream is closed
+stream.on('close', function() {
+	console.log("Indexed " + count + " documents");
+});
+
+stream.on('error', function(err) {
+	console.log(err);
+});
+
+router.post('/search', function(req, res, next) {
+	res.redirect('/search?/q=' + req.body.q);
+});
+
+router.post('/search', function(req, res, next) {
+	if (req.query.q) {
+		Product.search({
+			query_string: {
+				query: req.query.q
+			}
+		}, function(err, results) {
+			if (err) return next(err);
+			var data = results.hits.hits.map(function(hit) {
+				return hit;
+			});
+
+			res.render('main/search-result', {
+				query: req.query.q,
+				data:data
+			});
+
+		});
+	}
+});
+
 router.get('/', function(req, res) {
 	res.render('main/home');
 });
@@ -25,9 +79,11 @@ router.get('/products/:id', function(req, res, next) {
 });
 
 
-router.get('/product/:id', function (req,res, next) {
-	Product.findById({_id: req.params.id }, function(err,product) {
-		if(err) return next(err);
+router.get('/product/:id', function(req, res, next) {
+	Product.findById({
+		_id: req.params.id
+	}, function(err, product) {
+		if (err) return next(err);
 		res.render('main/product', {
 			product: product
 		});
